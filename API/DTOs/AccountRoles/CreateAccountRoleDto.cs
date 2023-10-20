200bf0f2908a645c70ddc07391234e82be647de0
Mohammad Rizki Atoilah <rizkysegitarius12@gmail.com>
2023-10-20 23:40:04 +0700

    Add-CRUD-AccountRole
    Add AccountRole = Contracts, Repository, Dto, Controller

---
+++ API/DTOs/AccountRoles/CreateAccountRoleDto.cs
@@ -0,0 +1,25 @@
+﻿using API.Models;
+
+namespace API.DTOs.AccountRoles
+{
+    public class CreateAccountRoleDto
+    {
+        // Properti-properti yang akan digunakan dalam kelas CreateAccountRoleDto
+        public Guid AccountGuid { get; set; }
+        public Guid RoleGuid { get; set; }
+
+        // Konversi Implisit (Implicit Conversion):
+        // Metode ini akan mengonversi objek CreateAccountRoleDto menjadi objek AccountRole secara implisit jika diperlukan.
+        public static implicit operator AccountRole(CreateAccountRoleDto dto)
+        {
+            // Membuat objek AccountRole baru dan mengisi propertinya dengan nilai dari objek CreateAccountRoleDto
+            return new AccountRole
+            {
+                AccountGuid = dto.AccountGuid,
+                RoleGuid = dto.RoleGuid,
+                CreatedDate = DateTime.Now, // Mengatur CreatedDate dengan waktu saat ini
+                ModifiedDate = DateTime.Now // Mengatur ModifiedDate dengan waktu saat ini
+            };
+        }
+    }
+}
---
+++ API/DTOs/Accounts/AccountDto.cs
@@ -0,0 +1,43 @@
+﻿using API.Models;
+using API.Utilities.Enums;
+
+namespace API.DTOs.Accounts;
+public class AccountDto
+{
+    // Properti-properti DTO untuk objek Account
+    public Guid Guid { get; set; } // GUID unik yang mengidentifikasi akun
+    public string Password { get; set; }
+    public int Otp { get; set; }
+    public StatusLevel Status { get; set; }
+    public bool IsUsed { get; set; }
+    public DateTime ExpiredTime { get; set; }
+
+    // Operator eksplisit digunakan untuk mengonversi objek Account ke AccountDto
+    public static explicit operator AccountDto(Account account)
+    {
+        return new AccountDto
+        {
+            Guid = account.Guid, // Mengisi properti Guid dengan nilai GUID dari objek Account yang diberikan.
+            Password = account.Password,
+            Otp = account.Otp,
+            Status = account.Status,
+            IsUsed = account.IsUsed,
+            ExpiredTime = account.ExpiredTime,
+        };
+    }
+
+    // Operator implisit digunakan untuk mengonversi objek AccountDto ke Account
+    public static implicit operator Account(AccountDto accountDto)
+    {
+        return new Account
+        {
+            Guid = accountDto.Guid, // Mengisi properti Guid dengan nilai GUID dari objek AccountDto yang diberikan.
+            Password = accountDto.Password,
+            Otp = accountDto.Otp,
+            Status = accountDto.Status,
+            IsUsed = accountDto.IsUsed,
+            ExpiredTime = accountDto.ExpiredTime,
+            ModifiedDate = DateTime.Now // Mengisi properti ModifiedDate dengan tanggal dan waktu saat ini.
+        };
+    }
+}
