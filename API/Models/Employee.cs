87c77a34448548901f0f9cdfb4fc2596566beda6
Mohammad Rizki Atoilah <rizkysegitarius12@gmail.com>
2023-10-20 18:04:45 +0700

    Add-Model-Enum
    Add Model Employee,Experience, BaseEntity - Enum Gender Level

---
+++ API/Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿using API.Utilities.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
+
+namespace API.Models
+{
+    [Table("tb_m_employees")]
+    public class Employee : BaseEntity
+    {
+        [Column("nik", TypeName = "nchar(6)")]
+        public string Nik { get; set; }
+
+        [Column("first_name", TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [Column("last_name", TypeName = "nvarchar(100)")]
+        public string? LastName { get; set; }
+
+        [Column("birth_date")]
+        public DateTime BirthDate { get; set; }
+
+        [Column("gender")]
+        public GenderLevel Gender { get; set; }
+
+        [Column("email", TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+
+        [Column("phone_number", TypeName = "nvarchar(20)")]
+        public string PhoneNumber { get; set; }
+
+        //kardinalitas
+        public ICollection<Experience>? Bookings { get; set; }
+    }
+}
---
+++ API/Models/Experience.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    [Table("tb_m_experiences")]
+    public class Experience
+    {
+        [Column ("name", TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+        [Column ("position", TypeName = "nvarchar(100)")]
+        public string Position { get; set; }
+        [Column ("company")]
+        public int Company { get; set; }
+        [Column ("employee_guid")]
+        public Guid EmployeeGuid { get; set; }
+
+        //kardinalitas
+        public Employee? Employee { get; set; }
+    }
+}
