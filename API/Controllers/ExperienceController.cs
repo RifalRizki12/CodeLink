2a49bba3e2977a944968933e6bc2ae4159175acb
Lailatul Qomariah <55173357+Lailatul-qomariah@users.noreply.github.com>
2023-10-21 10:08:53 +0700

    Add-CRUD-Experience
    -contract
    -repository
    -controller
    -DTOs
    -validator

---
+++ API/Controllers/ExperienceController.cs
@@ -0,0 +1,180 @@
+﻿using API.Contracts;
+using API.DTOs.Experiences;
+using API.Models;
+using API.Utilities.Handler;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExperienceController : ControllerBase
+    {
+        private readonly IExperienceRepository _experienceRepository;
+
+        public ExperienceController(IExperienceRepository experienceRepository)
+        {
+            _experienceRepository = experienceRepository;
+        }
+
+        // GET api/experience
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // Memanggil metode GetAll dari _experienceRepository untuk mendapatkan semua data experience.
+            var result = _experienceRepository.GetAll();
+
+            // Memeriksa apakah hasil query tidak mengandung data.
+            if (!result.Any())
+            {
+                // Mengembalikan respons Not Found jika tidak ada data experience.
+                return NotFound(new ResponseErrorHandler
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "Data Experience Not Found"
+                });
+            }
+
+            // Mengonversi hasil query ke objek DTO (Data Transfer Object) menggunakan Select.
+            var data = result.Select(x => (ExperienceDto)x);
+
+            // Mengembalikan data yang ditemukan dalam respons OK.
+            return Ok(new ResponseOKHandler<IEnumerable<ExperienceDto>>(data));
+        }
+
+        // GET api/experience/{guid}
+        [HttpGet("{guid}")]
+        public IActionResult GetByGuid(Guid guid)
+        {
+            // Memanggil metode GetByGuid dari _experienceRepository dengan parameter GUID.
+            var result = _experienceRepository.GetByGuid(guid);
+
+            // Memeriksa apakah hasil query tidak ditemukan (null).
+            if (result is null)
+            {
+                // Mengembalikan respons Not Found jika data experience dengan GUID tertentu tidak ditemukan.
+                return NotFound(new ResponseErrorHandler
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "Experience Data with Specific GUID Not Found"
+                });
+            }
+
+            // Mengonversi hasil query ke objek DTO (Data Transfer Object).
+            return Ok(new ResponseOKHandler<ExperienceDto>((ExperienceDto)result));
+        }
+
+        // POST api/experience
+        [HttpPost]
+        public IActionResult Create(CreateExperienceDto experienceDto)
+        {
+            try
+            {
+
+                // Memanggil metode Create dari _experienceRepository untuk membuat data experience baru.
+                var result = _experienceRepository.Create(experienceDto);
+
+                // Mengembalikan data yang berhasil dibuat dalam respons OK.
+                return Ok(new ResponseOKHandler<ExperienceDto>((ExperienceDto)result));
+            }
+            catch (ExceptionHandler ex)
+            {
+                // Mengembalikan respons server error jika terjadi kesalahan dalam proses.
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorHandler
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Status = HttpStatusCode.InternalServerError.ToString(),
+                    Message = "Failed to create data",
+                    Error = ex.Message
+                });
+            }
+        }
+
+        // PUT api/experience
+        [HttpPut]
+        public IActionResult Update(ExperienceDto expDto)
+        {
+            try
+            {
+                // Memeriksa apakah entitas experience yang akan diperbarui ada dalam database.
+                var entity = _experienceRepository.GetByGuid(expDto.Guid);
+                if (entity is null)
+                {
+                    // Mengembalikan respons Not Found jika experience dengan GUID tertentu tidak ditemukan.
+                    return NotFound(new ResponseErrorHandler
+                    {
+                        Code = StatusCodes.Status404NotFound,
+                        Status = HttpStatusCode.NotFound.ToString(),
+                        Message = " Experience with Specific GUID Not Found"
+                    });
+                }
+
+                // Menyalin nilai CreatedDate dari entitas yang ada ke entitas yang akan diperbarui.
+                Experience toUpdate = expDto;
+                toUpdate.CreatedDate = entity.CreatedDate;
+
+                // Memanggil metode Update dari _experienceRepository untuk memperbarui data experience.
+               _experienceRepository.Update(expDto);
+
+
+                // Mengembalikan pesan sukses dalam respons OK.
+                return Ok(new ResponseOKHandler<string>("Data Has Been Deleted"));
+            }
+            catch (ExceptionHandler ex)
+            {
+                // Mengembalikan respons server error jika terjadi kesalahan dalam proses.
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorHandler
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Status = HttpStatusCode.InternalServerError.ToString(),
+                    Message = "Failed to update data",
+                    Error = ex.Message
+                });
+            }
+        }
+
+        // DELETE api/experience/{guid}
+        [HttpDelete("{guid}")]
+        public IActionResult Delete(Guid guid)
+        {
+            try
+            {
+                // Memanggil metode GetByGuid dari _experienceRepository untuk mendapatkan entitas yang akan dihapus.
+                var entity = _experienceRepository.GetByGuid(guid);
+
+                // Memeriksa apakah entitas yang akan dihapus ada dalam database.
+                if (entity is null)
+                {
+                    // Mengembalikan respons Not Found jika experience tidak ditemukan.
+                    return NotFound(new ResponseErrorHandler
+                    {
+                        Code = StatusCodes.Status404NotFound,
+                        Status = HttpStatusCode.NotFound.ToString(),
+                        Message = "experience Not Found"
+                    });
+                }
+
+                // Memanggil metode Delete dari _experienceRepository untuk menghapus data experience.
+               _experienceRepository.Delete(entity);
+
+
+                // Mengembalikan kode status 204 (No Content) untuk sukses penghapusan tanpa respons.
+                return Ok(new ResponseOKHandler<string>("Data Has Been Deleted"));
+            }
+            catch (ExceptionHandler ex)
+            {
+                // Mengembalikan respons server error jika terjadi kesalahan dalam proses.
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorHandler
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Status = HttpStatusCode.InternalServerError.ToString(),
+                    Message = "Failed to delete experience",
+                    Error = ex.Message
+                });
+            }
+        }
+    }
+}
---
+++ API/Utilities/Validations/Experiences/ExperienceValidator.cs
@@ -0,0 +1,25 @@
+﻿using API.DTOs.Experiences;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Experiences;
+
+public class SkillValidator : AbstractValidator<ExperienceDto>
+{
+    public SkillValidator()
+    {
+        RuleFor(e => e.Guid)
+           .NotNull();
+
+        RuleFor(e => e.Name)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(e => e.Position)
+            .NotEmpty()
+            .MaximumLength(50);
+
+        RuleFor(e => e.Company)
+            .NotEmpty()
+            .MaximumLength(50);
+    }
+}
