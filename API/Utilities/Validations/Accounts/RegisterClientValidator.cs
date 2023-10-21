be70b09bdc2353c4672687d7f74a797ae429d6f1
Mohammad Rizki Atoilah <rizkysegitarius12@gmail.com>
2023-10-21 12:49:35 +0700

    Add-Validasi-Register_Company

---
+++ API/Utilities/Validations/Accounts/RegisterClientValidator.cs
@@ -0,0 +1,53 @@
+﻿using API.DTOs.Accounts;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Accounts
+{
+    public class RegisterClientValidator : AbstractValidator<RegisterClientDto>
+    {
+        public RegisterClientValidator()
+        {
+            // Aturan validasi untuk properti 'FirstName' dalam objek EmployeeDto
+            RuleFor(e => e.FirstName)
+                .NotEmpty()
+                .MinimumLength(3);  // Properti tidak boleh kosong
+
+            // Aturan validasi untuk properti 'Gender' dalam objek EmployeeDto
+            RuleFor(e => e.Gender)
+                .NotNull()     // Properti tidak boleh null
+                .IsInEnum();   // Properti harus merupakan nilai dari enum yang valid
+
+            // Aturan validasi untuk properti 'HiringDate' dalam objek EmployeeDto
+            RuleFor(employee => employee.HiringDate)
+                .NotNull().WithMessage("Hiring Date tidak boleh kosong.")
+                .Must(date => date != DateTime.MinValue).WithMessage("Hiring Date harus diisi.");
+
+            // Aturan validasi untuk properti 'Email' dalam objek EmployeeDto
+            RuleFor(e => e.Email)
+                .NotEmpty() // Properti tidak boleh kosong, dengan pesan kustom jika tidak terpenuhi
+                .EmailAddress().WithMessage("Format Email Salah");  // Properti harus merupakan alamat email yang valid, dengan pesan kustom jika tidak terpenuhi
+
+            // Aturan validasi untuk properti 'PhoneNumber' dalam objek EmployeeDto
+            RuleFor(e => e.PhoneNumber)
+                .NotEmpty().WithMessage("Phone Number tidak boleh kosong")         // Properti tidak boleh kosong
+                .MaximumLength(16);
+
+            RuleFor(e => e.NameCompany)
+                .NotEmpty()
+                .MinimumLength(3);
+
+            RuleFor(e => e.AddressCompany)
+                .NotEmpty()
+                .MinimumLength(4);
+            
+            RuleFor(e => e.Password)
+                .NotEmpty()         // Properti tidak boleh kosong
+                .MinimumLength(8);  // Panjang minimal 8 karakter
+
+            RuleFor(e => e.ConfirmPassword)
+                .NotEmpty()         // Properti tidak boleh kosong
+                .MinimumLength(8);  // Panjang minimal 8 karakter
+
+        }
+    }
+}
---
+++ API/Utilities/Validations/Companies/CompanyValidator.cs
@@ -0,0 +1,21 @@
+﻿using API.DTOs.Companies;
+using API.DTOs.Employees;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Companies
+{
+    public class CompanyValidator : AbstractValidator<CompanyDto>
+    {
+        public CompanyValidator()
+        {
+            
+            RuleFor(e => e.Name)
+                .NotEmpty()
+                .MinimumLength(3);
+
+            RuleFor(e => e.Address)
+                .NotEmpty()
+                .MinimumLength(4);
+        }
+    }
+}
