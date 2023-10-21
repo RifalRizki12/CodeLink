9c8e36c842b5edc9458ee2ebd4e9d2e1290536b5
Mohammad Rizki Atoilah <rizkysegitarius12@gmail.com>
2023-10-21 13:41:36 +0700

    Add-LoginJWT

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,10 +1,13 @@
 ﻿using API.Contracts;
 using API.DTOs.Accounts;
 using API.Models;
+using API.Repositories;
 using API.Utilities.Handler;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using System.Net;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -14,19 +17,89 @@ namespace API.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IAccountRepository _accountRepository;
+        private readonly IRoleRepository _roleRepository;
+        private readonly IAccountRoleRepository _accountRoleRepository;
         private readonly IEmployeeRepository _employeeRepository;
         private readonly IEmailHandler _emailHandler;
+        private readonly ITokenHandler _tokenHandler;
 
         // Konstruktor controller yang menerima IAccountRepository sebagai parameter.
-        public AccountController(IAccountRepository accountRepository, IEmployeeRepository employeeRepository, IEmailHandler emailHandler)
+        public AccountController(IAccountRepository accountRepository, IEmployeeRepository employeeRepository, IEmailHandler emailHandler, IRoleRepository roleRepository, IAccountRoleRepository accountRoleRepository, ITokenHandler tokenHandler)
         {
             _accountRepository = accountRepository;
             _employeeRepository = employeeRepository;
             _emailHandler = emailHandler;
+            _roleRepository = roleRepository;
+            _accountRoleRepository = accountRoleRepository;
+            _tokenHandler = tokenHandler;
         }
 
-        // Metode untuk mengirim OTP melalui email dalam kasus lupa kata sandi
-        [HttpPut("forgot-password")]
+        //Login
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDto request)
+        {
+            try
+            {
+                // Validasi input data menggunakan ModelState
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(new ResponseErrorHandler
+                    {
+                        Code = StatusCodes.Status400BadRequest,
+                        Status = HttpStatusCode.BadRequest.ToString(),
+                        Message = "Invalid input !"
+                    });
+                }
+
+                // Cari pengguna (akun) berdasarkan alamat email
+                var user = _accountRepository.GetByEmployeeEmail(request.Email);
+                var employee = _employeeRepository.GetByEmployeeEmail(request.Email);
+
+                if (user == null || !HashHandler.VerifyPassword(request.Password, user.Password))
+                {
+                    return BadRequest(new ResponseErrorHandler
+                    {
+                        Code = StatusCodes.Status400BadRequest,
+                        Status = HttpStatusCode.BadRequest.ToString(),
+                        Message = "Account or Password is invalid !",
+                    });
+                }
+
+                var claims = new List<Claim>();
+                claims.Add(new Claim("Email", employee.Email));
+                claims.Add(new Claim("Fullname", string.Concat(employee.FirstName + " " + employee.LastName)));
+                var getRoleName = from ar in _accountRoleRepository.GetAll()
+                                  join r in _roleRepository.GetAll() on ar.RoleGuid equals r.Guid
+                                  where ar.AccountGuid == user.Guid
+                                  select r.Name;
+
+                foreach (var roleName in getRoleName)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, roleName));
+                }
+
+                var generateToken = _tokenHandler.Generate(claims);
+
+                // Jika validasi berhasil, kirim respons OK dengan pesan login berhasil
+                return Ok(new ResponseOKHandler<object>("Login Success", new { Token = generateToken }));
+            }
+            catch (Exception ex)
+            {
+                // Tangani pengecualian dan kembalikan respons 500 Internal Server Error
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorHandler
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Status = HttpStatusCode.InternalServerError.ToString(),
+                    Message = "Error during login",
+                    Error = ex.Message
+                });
+            }
+        }
+
+    }
+
+    // Metode untuk mengirim OTP melalui email dalam kasus lupa kata sandi
+    [HttpPut("forgot-password")]
         [AllowAnonymous]
         public IActionResult ForgotPassword(string email)
         {
@@ -40,7 +113,7 @@ namespace API.Controllers
                 {
                     Code = StatusCodes.Status404NotFound,
                     Status = HttpStatusCode.NotFound.ToString(),
-                    Message = "Data Tidak Ditemukan"
+                    Message = "Data Not Found !"
                 });
             }
 
@@ -50,7 +123,7 @@ namespace API.Controllers
                 {
                     Code = StatusCodes.Status400BadRequest,
                     Status = HttpStatusCode.BadRequest.ToString(),
-                    Message = "Invalid input data."
+                    Message = "Invalid input data !"
                 });
             }
 
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,16 @@
+using API.Contracts;
 using API.Data;
+using API.Repositories;
+using API.Utilities.Handler;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,10 +18,96 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<CodeLinkDbContext>(option => option.UseSqlServer(connectionString));
 
+builder.Services.AddScoped<ITokenHandlers, API.Utilities.Handler.TokenHandler>();
+
+// Add repositories to the container.
+// Mendaftarkan AccountRepository sebagai implementasi IAccountRepository.
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IAccountRoleRepository, AccountRoleRepository>();
+builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IExperienceRepository, ExperienceRepository>();
+builder.Services.AddScoped<IExperienceSkillRepository, ExperienceSkillRepository>();
+builder.Services.AddScoped<IRatingRepository, RatingRepository>();
+builder.Services.AddScoped<IRatingRepository, RatingRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<ISalaryRepository, SalaryRepository>();
+builder.Services.AddScoped<ISkillRepository, SkillRepository>();
+builder.Services.AddScoped<ITestRepository, TestRepository>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x => {
+    x.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Code Link",
+        Description = "ASP.NET Core API 6.0"
+    });
+    x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+    x.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
+// Mengkonfigurasi layanan untuk pengendalian API (controllers)
+builder.Services.AddControllers()
+       .ConfigureApiBehaviorOptions(options =>
+       {
+           // Konfigurasi respons kustom untuk validasi yang gagal
+           options.InvalidModelStateResponseFactory = context =>
+           {
+               // Mengambil semua pesan kesalahan validasi dari ModelState
+               var errors = context.ModelState.Values
+                                   .SelectMany(v => v.Errors)
+                                   .Select(v => v.ErrorMessage);
+
+               // Mengembalikan respons HTTP 400 Bad Request dengan pesan kesalahan validasi
+               return new BadRequestObjectResult(new ResponseValidatorHandler(errors));
+           };
+       });
+
+//Add FluentValidation Service
+builder.Services.AddFluentValidationAutoValidation()
+    .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+// JWT Authentication Configuration
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+       .AddJwtBearer(options =>
+       {
+           options.RequireHttpsMetadata = false; // for development only
+           options.SaveToken = true;
+           options.TokenValidationParameters = new TokenValidationParameters
+           {
+               ValidateIssuer = true,
+               ValidIssuer = builder.Configuration["JWTService:Issuer"],
+               ValidateAudience = true,
+               ValidAudience = builder.Configuration["JWTService:Audience"],
+               IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTService:SecretKey"])),
+               ValidateLifetime = true,
+               ClockSkew = TimeSpan.Zero
+           };
+       });
 
 var app = builder.Build();
 
@@ -22,6 +119,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
