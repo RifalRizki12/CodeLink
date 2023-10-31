e0712686d6380242787887e722f38138dd209002
Mohammad Rizki Atoilah <rizkysegitarius12@gmail.com>
2023-10-31 16:35:21 +0700

    Add-ViewHomeClient
    Add Controller, Repository, contracts HomeClient

---
+++ CLIENT/Controllers/HomeClientController.cs
@@ -0,0 +1,31 @@
+﻿using API.DTOs.Accounts;
+using API.DTOs.Employees;
+using API.DTOs.Roles;
+using API.Models;
+using CLIENT.Contract;
+using CLIENT.Models;
+using CLIENT.Repository;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Core.Types;
+using System.Diagnostics;
+using System.Net;
+
+namespace CLIENT.Controllers
+{
+    public class HomeClientController : Controller
+    {
+
+        private readonly IHomeClientRepository _homeClientRepository;
+
+        public HomeClientController(IHomeClientRepository homeClientRepository)
+        {
+            _homeClientRepository = homeClientRepository;
+        }
+
+        public IActionResult HomeClient()
+        {
+            return View();
+        }
+
+    }
+}
