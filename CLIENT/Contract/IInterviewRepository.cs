d427a5f74b95730bb2d07b0b38eadaa751cfddef
Ahmad Sulthon Al Ayyubi <dafiqalimi17@gmail.com>
2023-11-02 21:44:04 +0700

    Update IInterviewRepository.cs

--- CLIENT/Contract/IInterviewRepository.cs
+++ CLIENT/Contract/IInterviewRepository.cs
@@ -13,6 +13,7 @@ namespace CLIENT.Contract
         Task<ResponseOKHandler<ScheduleInterviewDto>> ScheduleUpdate(Guid guid, ScheduleInterviewDto scheduleUpdate);
         Task<ResponseOKHandler<IEnumerable<InterviewDto>>> GetAllInterview();
         Task<ResponseOKHandler<IEnumerable<GetInterviewDto>>> GetByCompanyGuid(Guid guid);
+        Task<ResponseOKHandler<AnnouncmentDto>> UpdateAnnouncement(Guid guid, AnnouncmentDto announcment);
         Task<ResponseOKHandler<IEnumerable<GetOnsiteDto>>> GetOnsite(Guid guid);
         Task<ResponseOKHandler<IEnumerable<GetIdleHistoryDto>>> GetIdleHistory(Guid guid);
     }
