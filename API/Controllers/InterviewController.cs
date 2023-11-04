142a36ec0eda1ffba848b56c5029951781d3ef28
Ahmad Sulthon Al Ayyubi <dafiqalimi17@gmail.com>
2023-11-04 19:19:38 +0700

    Solve Rating

--- API/Controllers/InterviewController.cs
+++ API/Controllers/InterviewController.cs
@@ -560,6 +560,7 @@ public class InterviewController : ControllerBase
     {
         var interviews = _interviewRepository.GetAllByClientGuid(companyGuid);
         var employees = _employeeRepository.GetAll();
+        var ratings = _ratingRepository.GetAll();
 
         if (!interviews.Any())
         {
@@ -572,12 +573,9 @@ public class InterviewController : ControllerBase
         }
 
         var idleHistoryDto = (from interview in interviews
+                              join rat in ratings on interview.Guid equals rat.Guid
                               join emp in employees on interview.EmployeeGuid equals emp.Guid
-                              where (emp.StatusEmployee == StatusEmployee.idle ||
-                                     emp.StatusEmployee == StatusEmployee.onsite) &&
-                                    (interview.StatusIntervew == StatusIntervew.ContarctTermination ||
-                                     interview.StatusIntervew == StatusIntervew.EndContract) &&
-                                    interview.EndContract.HasValue
+                              where interview.EndContract.HasValue
                               select new GetIdleHistoryDto
                               {
                                   EmployeeGuid = emp.Guid,
@@ -586,7 +584,7 @@ public class InterviewController : ControllerBase
                                   Foto = emp.Foto,
                                   StartContract = interview.StartContract,
                                   EndContract = interview.EndContract,
-                                  Status = emp.StatusEmployee.ToString()
+                                  Rate = rat.Rate.ToString()
                               }).ToList();
 
         return Ok(new ResponseOKHandler<IEnumerable<GetIdleHistoryDto>>(idleHistoryDto));
